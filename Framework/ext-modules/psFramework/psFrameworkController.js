9df7bf0be42dd4840d5d142e4cf21ca83e6a4033
jeffersonshibuya <jeffersonshibuya@yahoo.com.br>
2015-08-06 14:33:34 -0300

    Dashboard

--- Framework/ext-modules/psFramework/psFrameworkController.js
+++ Framework/ext-modules/psFramework/psFrameworkController.js
@@ -1,15 +1,15 @@
 ﻿"use strict";
 
 angular.module("psFramework").controller("psFrameworkController",
-    ['$scope', '$rootScope', '$window', '$timeout',
-        function ($scope, $rootScope, $window, $timeout) {
+    ['$scope', '$rootScope', '$window', '$timeout', '$location',
+        function ($scope, $rootScope, $window, $timeout, $location) {
 
             $scope.isMenuVisible = true;
             $scope.isMenuButtonVisible = true;
             $scope.isMenuVertical = true;
 
             $scope.$on('ps-menu-item-selected-event', function (evt, data) {
-                $scope.routeString = data.route;
+                $location.path(data.route);
                 checkWidth();
                 broadcastMenuState();
             });
